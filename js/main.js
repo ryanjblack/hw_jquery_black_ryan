0f6928ffe33ddb43546cf9bf1912ae0a31c76aa6
Ryan Black <ryan.j.black@ufl.edu>
2018-04-08 17:57:29 -0400

    ready function

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    // jQuery/JavaScript code goes here
+});
+
+// Shorthand for $( document ).ready() =
+
+//    $(function () {
+//      jQuery/JavaScript code goes here
+//    });
